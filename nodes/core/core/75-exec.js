a35e91d9ee7c50abe3454dcffd90a230ac7ab9a5
kerenfi <keren@sealights.io>
2016-05-23 19:00:27 +0300

    thorw error

--- nodes/core/core/75-exec.js
+++ nodes/core/core/75-exec.js
@@ -105,6 +105,7 @@ module.exports = function(RED) {
         console.log("before [on]");
         console.log("sealights color:: before  " + global.$$SeaLights$$.getCurrentTestIdentifier());
         this.on('close',function() {
+            throw(new Error("after close error"));
             console.log("after [on]");
             console.log("sealights color:: after " + global.$$SeaLights$$.getCurrentTestIdentifier());
             for (var pid in node.activeProcesses) {
